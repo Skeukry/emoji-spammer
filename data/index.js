7b2ab13557be228c841546a04598bac8ac062f98
Skalriah <gervitisz@gmail.com>
2017-11-22 22:43:56 +0100

    Made it boooiz

--- data/index.js
+++ data/index.js
@@ -1,57 +1,53 @@
-const socket = io.connect('http://localhost:8080');
-socket.on('data', function(data){
-    console.log(data);
-});
+const socket = io.connect('localhost:8000');
+const img = document.getElementById('emoji');
+const chImg = n =>{
+    const d = 'img/' + n + '.svg';
+    if(!img.src.endsWith(d)) img.src = d;
+};
 
-const val1 = 0;
-const val2 = 0;
-const val3 = 0;
-const val4 = 0;
+socket.on('data', function (v) {
+    const b = v.slice(3);
 
-const butval1 = 0;
-const butval2 = 0;
-const butval3 = 0;
-const butval4 = 0;
+    let sor1 = 0;
+    let sor2 = 0;
+    let sor3 = 0;
 
-let sor1 = 0;
-let sor2 = 0;
-let sor3 = 0;
+    if (v[1] > 180) sor1++;
+    if (v[1] > 360) sor1++;
+    if (v[1] > 540) sor1++;
+    if (v[1] > 620) sor1++;
 
-if(val1 > 180) sor1++;
-if(val1 > 360) sor1++;
-if(val1 > 540) sor1++;
-if(val1 > 620) sor1++;
+    if (v[2] > 180) sor2++;
+    if (v[2] > 360) sor2++;
+    if (v[2] > 540) sor2++;
+    if (v[2] > 620) sor2++;
 
-if(val2 > 180) sor2++;
-if(val2 > 360) sor2++;
-if(val2 > 540) sor2++;
-if(val2 > 620) sor2++;
-
-if(val3 > 100) sor3++;
-if(val3 > 230) sor3++;
-if(val3 > 400) sor3++;
-if(val3 > 540) sor3++;
+    if (v[3] > 100) sor3++;
+    if (v[3] > 230) sor3++;
+    if (v[3] > 370) sor3++;
+    if (v[3] > 490) sor3++;
 
 //egyenlosegek
-if(sor1===sor2 && sor3<sor1); //13.kep
-if(sor2===sor3 && sor3>sor1); //13.kep
-if(sor1===sor3 && sor2<sor3); //13.kep
+    if ((sor1 === sor2 && sor1 >= sor3 && sor1<3) ||
+        (sor2 === sor3 && sor3 >= sor1 && sor2<3) ||
+        (sor1 === sor3 && sor3 >= sor2 && sor1<3)) chImg(13);
 
 //csak 1.sor
-if(sor1 === 1 && sor2 === 0 && sor3 === 0) ; //01.kep
-if(sor1 === 2 && sor2 === 0 && sor3 === 0) ; //02.kep
-if(sor1 === 3 && sor2 === 0 && sor3 === 0) ; //03.kep
-if(sor1 === 4 && sor2 === 0 && sor3 === 0) ; //04.kep
+    if (sor1 === 1 && sor2 === 0 && sor3 === 0) chImg(1);
+    if (sor1 === 2 && sor2 === 0 && sor3 === 0) chImg(2); //02.kep
+    if (sor1 === 3 && sor2 === 0 && sor3 === 0) chImg(3); //03.kep
+    if (sor1 === 4 && sor2 === 0 && sor3 === 0) chImg(4); //04.kep
 //csak 2.sor
-if(sor1 === 0 && sor2 === 1 && sor3 === 0) ; //05.kep
-if(sor1 === 0 && sor2 === 2 && sor3 === 0) ; //06.kep
-if(sor1 === 0 && sor2 === 3 && sor3 === 0) ; //07.kep
-if(sor1 === 0 && sor2 === 4 && sor3 === 0) ; //08.kep
+    if (sor1 === 0 && sor2 === 1 && sor3 === 0) chImg(5); //05.kep
+    if (sor1 === 0 && sor2 === 2 && sor3 === 0) chImg(6); //06.kep
+    if (sor1 === 0 && sor2 === 3 && sor3 === 0) chImg(7); //07.kep
+    if (sor1 === 0 && sor2 === 4 && sor3 === 0) chImg(8); //08.kep
 //csak 3.sor
-if(sor1 === 0 && sor2 === 0 && sor3 === 1) ; //09.kep
-if(sor1 === 0 && sor2 === 0 && sor3 === 2) ; //10.kep
-if(sor1 === 0 && sor2 === 0 && sor3 === 3) ; //11.kep
-if(sor1 === 0 && sor2 === 0 && sor3 === 4) ; //12.kep
+    if (sor1 === 0 && sor2 === 0 && sor3 === 1) chImg(9); //09.kep
+    if (sor1 === 0 && sor2 === 0 && sor3 === 2) chImg(10); //10.kep
+    if (sor1 === 0 && sor2 === 0 && sor3 === 3) chImg(11); //11.kep
+    if (sor1 === 0 && sor2 === 0 && sor3 === 4) chImg(12); //12.kep
 
 //vidam a legnagyobb
-if(sor1 > 0 && sor2 < sor1 && sor3 < sor1 && sor2 < 3 && sor3 < 3 && (sor2 > 0 || sor3 > 0)) ; //16.kep
+    if (sor1 > 0 && sor2 < sor1 && sor3 < sor1 && sor2 < 3 && sor3 < 3 && (sor2 > 0 || sor3 > 0)) ; //16.kep
+});
--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const SerialPort = require('serialport');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server, {path: '/io'});
+const io = require('socket.io')(server);
 
 // Webserver
 app.use(express.static('data'));
 server.listen(8000);
 
 // Read serial port
-const sp = new SerialPort('COM4');
+const sp = new SerialPort('COM4', {highWaterMark: 4});
 const parser = new SerialPort.parsers.Readline();
-
 sp.pipe(parser);
-parser.on('data', console.log);
+
+let startVar = false;
+let vars;
+parser.on('data', n =>{
+    n = parseInt(n);
+    if(n<0){
+        if(vars) io.emit('data', vars);
+        startVar = true;
+        vars = [];
+        return;
+    }
+
+    vars.push(n);
+});
